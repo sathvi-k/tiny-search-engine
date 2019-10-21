1ab33494cf153bf68797482f3bda5e93ed40ebb4
johnbkariuki <56233847+johnbkariuki@users.noreply.github.com>
2019-10-21 19:17:57 -0400

    Add files via upload

---
+++ crawlerf.c
@@ -0,0 +1,214 @@
+/* crawler.c --- 
+ * 
+ * 
+ * Author: Sathvika R. Korandla
+ * Created: Thu Oct 17 16:44:08 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <queue.h>
+#include <hash.h>
+#include <webpage.h>
+#include <sys/stat.h>
+
+void print_anything(void *elementp){                                       
+  webpage_t *wp=(webpage_t*)elementp;
+  printf("URL: %s\n", webpage_getURL(wp));
+}
+
+bool searchfn(void* elementp,const void* searchkeyp){                     
+  char *wp2=(char*)elementp;                                     
+  char *urlP = (char*)searchkeyp;                                          
+                                          
+  if(strcmp(urlP,wp2)==0){                                              
+    return true;                                                           
+  }
+	
+  else{                                                                    
+    return false;                                                          
+  }                                                                        
+}     
+
+
+
+int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
+	
+  //make the dirname if it doesnt exist
+
+  char dir_path[50];
+  char *path= "../";
+	
+  sprintf(dir_path,"%s%s",path,dirname);  
+  struct stat dirstat;
+  
+  if (stat(dir_path,&dirstat) == -1) {
+    mkdir (dir_path, 0700);
+  }
+
+  //open the file
+
+  char file_path[50];
+  sprintf(file_path,"%s/%d",dir_path,id);
+  FILE *my_file = fopen(file_path, "w");
+
+
+  // write the four lines
+
+  char *url=webpage_getURL(pagep);
+	fprintf(my_file,"%s\n",url);
+	//free(url);
+
+  int depth=webpage_getDepth(pagep);
+  fprintf(my_file,"%d\n",depth);
+
+  int html_len=webpage_getHTMLlen(pagep);
+	fprintf(my_file,"%d\n",html_len);
+
+  char *html=webpage_getHTML(pagep);
+  fprintf(my_file,"%s\n",html);
+	//free(html);
+
+  //close the file
+  fclose(my_file);
+
+  return 0;
+
+}
+
+
+
+int main(int argc,char *argv[]){
+	
+	if(argc==4){
+		//gotten from arguments
+		char *url=argv[1];
+		char *pagedir=argv[2];
+		int maxdepth=atoi(argv[3]); 
+		
+		//track ID and depth
+		int id=1;
+		
+		//check if provided depth is greater than or equal to 1
+		if(maxdepth>=0){
+			queue_t *webq=qopen();                                                                                                             
+			hashtable_t *urlH=hopen(20);
+			
+			// create webpage, put it in a queue and the URL in hash
+			//then save it in a file
+			webpage_t *page=webpage_new(url,0,NULL);
+			
+			char seedurl[1000];
+			strcpy(seedurl,webpage_getURL(page));
+			const char *surl=seedurl;
+			
+			hput(urlH,(void*)surl, surl, sizeof(*surl));
+			qput(webq,(void*)page);
+			
+			
+			//check if queue if empty if not carry on
+			while((page=qget(webq))!=NULL){
+				
+				if(webpage_fetch(page)){
+					
+					//check if depth exceeds the max depth if not carry on
+					if(webpage_getDepth(page)<=maxdepth){
+
+						printf("saved:%s\n",webpage_getURL(page));
+						
+						pagesave(page,id,pagedir);
+						id=id+1;
+						
+						int pos = 0;
+						char *result=NULL;
+						webpage_t* inter_page=NULL;                                                                                                         
+						
+						while((pos=webpage_getNextURL(page,pos,&result)) > 0){
+							
+							if(IsInternalURL(result)){                                                                                                       
+								
+								//printf("Found internal url: %s\n", result);
+								
+								if(hsearch(urlH,searchfn,result,sizeof(*result))==NULL){
+									
+									inter_page=webpage_new(result,(webpage_getDepth(page))+1, NULL);
+
+									char inturl[1000];
+									strcpy(inturl,result);
+									const char *iurl=inturl;
+									
+									hput(urlH,(void*)iurl, iurl, sizeof(*iurl));
+									qput(webq,(void*)inter_page);	
+								}
+								
+							}                                                                                                                   
+							else{                                                                                                            
+								//	printf("Found external url: %s\n", result);                                                                      
+							}
+							
+							free(result);                                                                                                        
+							result=NULL;                                                                                                        
+						}		                                                                                                                  
+					}
+				}
+				webpage_delete(page);
+				page=NULL;
+			}
+			qclose(webq);
+			hclose(urlH);
+			exit(EXIT_SUCCESS);
+		}
+		else{
+			printf("Maxdepth is negative, must be >=0\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else{
+		printf("Usage:crawlerf <seedurl> <pagedir> <maxdepth>\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	
+	                                                                                                                            
+                                                                                                                                      
