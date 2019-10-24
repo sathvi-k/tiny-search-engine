3263f5c9de06ca19e88a9b533e344c7bc5f9d2a0
Ayazhan Abdiken <ayazhan.abdiken.22@dartmouth.edu>
2019-10-24 04:23:43 -0400

    step 3 finished, but needs to be run

--- indexer/indexer3.c
+++ indexer/indexer3.c
@@ -17,6 +17,32 @@
 #include <webpage.h>
 #include <pageio.h>
 #include <ctype.h>
+#include <queue.h>
+#include <hash.h>
+
+typedef struct nwordc{
+  char *norm_word;
+  int count;
+}nwordc_t;
+
+int get_count(void *elementp){
+  nwordc_t *wrdp=(nwordc_t*)elementp;
+  int num=wrdp->count;
+  return num;
+}
+
+bool searchfn(void* elementp,const void* searchkeyp){
+  nwordc_t *wrdp2=(nwordc_t*)elementp;
+  char *wordP = (char*)searchkeyp;
+  char *wrdp2_word=wrdp2->norm_word;
+
+  if(strcmp(wordP,wrdp2_word)==0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
 /* NormalizeWord()
  *
@@ -25,41 +51,64 @@
  */
 
 char* NormalizeWord(char *word){
-	/* char word_a[30];
+  /* char word_a[30];
   strcpy(word_a,word);
   char *normalized=(char *)calloc(1, strlen(word_a) + 1);
-	*/
-	char *normalized=NULL;
-	int p=strlen(word);
+  */
+
+  char *normalized=NULL;
+  int p=strlen(word);
   if (p>=3){
     for (int i=0; i<p; i++){
       if (isalpha(word[i])!=0){
-				word[i]=tolower(word[i]);
+	word[i]=tolower(word[i]);
       }
       else{
-				return normalized;
+	return normalized;
       }
     }
     //strcpy(normalized,word_a);
-		normalized=word;
-	}
-	return normalized;
+    normalized=word;
+  }
+  return normalized;
 }
 
 
 int main(void){
-	webpage_t *loaded;
-	loaded=pageload(1,"pages");
+  webpage_t *loaded;
+  loaded=pageload(1,"pages");
 
-	int pos=0;
-	char *word;
-	while((pos=webpage_getNextWord(loaded,pos,&word))>0){
-	  if (NormalizeWord(word)!=NULL){
-	    printf("%s\n",word);
-		}
-	  free(word);
-	}
-	webpage_delete(loaded);
-	loaded=NULL;
-	return 0;
+  int pos=0;
+  char *word;
+
+  hashtable_t *wordH=hopen(150);
+  while((pos=webpage_getNextWord(loaded,pos,&word))>0){
+    if (NormalizeWord(word)!=NULL){
+      //printf("%s\n",word);
+
+      nwordc_t *w_obj;
+      w_obj->norm_word=word;
+      w_obj->count=0;
+  
+      //if there's NO word object with the norm_word=word, put this object into
+      //the hashtable
+      if (hsearch(wordH,searchfn,word,strlen(word))==NULL){	
+	w_obj->count=1;
+	hput(wordH,(void*)w_obj,word,strlen(word));
+      }
+      //if there IS word object in the hashtable with
+      //its norm_word attribute equal to the value of "word" variable
+      else{
+	nwordc_t hword=(nwordc_t*) hsearch(wordH,searchfn,word,strlen(word));
+	hword->count+=1;
+      }
+    }
+    free(word);
+  }
+  int sum=0;
+  sum+=happly(wordH,get_count);
+  hclose(wordH);
+  webpage_delete(loaded);
+  loaded=NULL;
+  exit(EXIT_SUCCESS);
 }
