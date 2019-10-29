80f218f1c6896962747bf62d34c941a8cd7def79
Sathvika Korandla <sathvika.korandla@gmail.com>
2019-10-28 20:03:25 -0400

    indexio.h

---
+++ utils/indexio.h
@@ -0,0 +1,32 @@
+#pragma once
+/* indexio.h --- 
+ * 
+ * 
+ * Author: Sathvika R. Korandla
+ * Created: Mon Oct 28 17:57:26 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include <stdio.h>                                                             
+#include <stdlib.h>                                                            
+#include <string.h>                                                            
+#include <inttypes.h>                                                          
+#include <unistd.h>                                                            
+#include <webpage.h>
+#include <pageio.h>
+
+typedef void index_t;
+
+/*                                                                             
+ * indexsave -- save the index to a file named indexnm                         
+ * returns: 0 for success; nonzero otherwise                 
+ */                                                                            
+int32_t indexsave(index_t *indexp, char *indexnm);                     
+                                                                               
+/*                                                                             
+ * indexload -- loads the index in <indexnm> into a new index                  
+ * returns: non-NULL for success; NULL otherwise                               
+ */                                                                            
+index_t *indexload(char *indexnm);
