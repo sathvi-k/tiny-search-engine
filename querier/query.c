e50c236585d6bd96b7a3798bb482f8a0617841ac
Sathvika Korandla <sathvika.korandla@gmail.com>
2019-10-31 22:47:12 -0400

    added querier folder, query.c, and a Makefile

---
+++ querier/query.c
@@ -0,0 +1,17 @@
+/* query.c --- 
+ * 
+ * 
+ * Author: Sathvika R. Korandla
+ * Created: Thu Oct 31 22:29:05 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+#include <stdio.h>
+
+int main(void){
+	printf("hello!\n");
+	return 0;
+}
