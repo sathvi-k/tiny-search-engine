0229b1378d9be26672a21a47d2683716bf344b0b
Sathvika Korandla <sathvika.korandla@gmail.com>
2019-11-02 17:29:12 -0400

    step 1 so far

--- querier/query.c
+++ querier/query.c
@@ -15,31 +15,43 @@ int main(void){
 	char input[50];
 	printf("> ");
 	int returned;
+	int j; // keep track of if there are numbers/punctuation (0 if no, 1 if yes)
 	while((returned=scanf(" %[^\n]", input))==1){
 		// split user's input by spaces and tabs
 		char *token = strtok(input, " \t");
+		char output[50]="";
 		while (token != NULL){
 			// if user enters ^D, break out of loop and stop scanning input
 			if (returned == EOF){
 				break;
 			}
-
-			// take string user enters and check that it only has alphabetical
-			// characters, and convert to lowercase letters
+			// take word from string user enters and check that it only
+			// has alphabetical characters, and convert to lowercase letters
 			for (int i=0;i<strlen(token);i++){
 				if (isalpha(token[i])!=0){
 					token[i]=tolower(token[i]);
+					j=0;
 				}
 				//otherwise, there's numbers and punctuations, so it's invalid
-				else{
-					printf("[invalid query]");
+				else if (isalpha(token[i])==0){
+					j=1;
 				}
 			}
-			// after converting to lowercase, print word, then move pointer to next
-			printf("%s ", token);
+
+			if (j==0){
+				sprintf(output,"%s%s ",output, token);
+			}
+			// after converting to lowercase, print word, then move pointer
+			// to next word
 			token = strtok(NULL, " \t");
 		}
 		// print newline and > to prompt user for input
+		if (j==0){
+			printf("%s",output);
+		}
+		else if (j==1){
+			printf("[invalid query]");
+		}
 		printf("\n> ");
 	}
 	return 0;
